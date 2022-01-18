cc97fd67a2a56fe90a6b93f6a4148572d849533b
Alvax10 <alvaro695547@gmail.com>
2022-01-18 01:45:08 -0300

    fixing heroku errors

--- fe-src/state.ts
+++ fe-src/state.ts
@@ -7,7 +7,6 @@ const state = {
         petName: '',
         email: '',
         _geoloc: {
-            name: '',
             lat: 0,
             lng: 0,
         },
@@ -29,81 +28,68 @@ const state = {
     },
     async sendEmailWithInfo(newLocation, petName, OtherUserEmail, userEmail, numeroDelUsuario) {
 
-        try {
+        const sendEmailToUser = await fetch(API_BASE_URL + "/send-email-to-user", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ newLocation, petName, OtherUserEmail, userEmail, numeroDelUsuario }),
+        })
+        .then((res) => { return res.json() })
+        .then((data) => {
 
-            const sendEmailToUser = await fetch(API_BASE_URL + "/send-email-to-user", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ newLocation, petName, OtherUserEmail, userEmail, numeroDelUsuario }),
-            })
-            .then((res) => { return res.json(); })
-            .then((data) => { 
-                console.log("Esta es la data de enviar el email: ", data);
-            })
-            // .catch((err) => {
-            //     console.log("Este es el error de send email: ", err);
-            // });
+            console.log(data);
+            console.log("Email enviado! :D");
+        })
+        .catch((err) => {
 
-        } catch (err) {
-            console.log("Este es el error al enviar el mail: ", err);
-        }
+            console.log("Este es el error de send email: ", err);
+        });
     },
     async mascotCloseFrom(callback) {
         const currentState = this.getState();
         const lat = currentState["_geoloc"]["lat"];
         const lng = currentState["_geoloc"]["lng"];
 
-        try {
+        const mascotsCloseFrom = await fetch(API_BASE_URL + "/mascots-close-from" + "?lat=" + lat + "&lng=" + lng, {
+        })
+        .then((res) => { return res.json() })
+        .then((data) => {
 
-            const mascotsCloseFrom = await fetch(API_BASE_URL + "/mascots-close-from" + "?lat=" + lat + "&lng=" + lng, {
-            })
-            .then((res) => { return res.json() })
-            .then((data) => {
-
-                console.log("Esta es la data de mascotas cerca de: ", data);
-                currentState["lostPetsAround"] = data;
-            })
-            // .catch((err) => {
-            //     console.log("Este es el error de mascots close from: ", err);
-            // });
-            callback();
-
-        } catch (err) {
+            console.log("Esta es la data de mascotas cerca de: ", data);
+            currentState["lostPetsAround"] = data;
+        })
+        .catch((err) => {
 
             console.log("Este es el error de mascots close from: ", err);
-        }
+        });
+        callback();
 
     },
     async reportLostPet(petName, ImageDataURL, _geoloc, callback) {
         const currentState = this.getState();
         const email = currentState["email"];
         
-        try {
+        const reportedPet = await fetch(API_BASE_URL + "/report/mascot", {
+           method: 'POST',
+           headers: {
+            'Content-Type': 'application/json',
+           },
+           body: JSON.stringify({ petName, _geoloc, ImageDataURL, email }),
+        })
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            
+           console.log(data);
+        })
+        .catch((err) => {
 
-            const reportedPet = await fetch(API_BASE_URL + "/report/mascot", {
-               method: 'POST',
-               headers: {
-                'Content-Type': 'application/json',
-               },
-               body: JSON.stringify({ petName, _geoloc, ImageDataURL, email }),
-            })
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-               console.log(data);
-            })
-            // .catch((err) => {
-            //     console.log("Este es el error de report mascot: ", err);
-            // });
-            callback();
-
-        } catch (err) {
             console.log("Este es el error de report mascot: ", err);
-        }
+        });
 
+        callback();
     },
     async allReportedPetsByAUser(callback) {
         const currentState = this.getState();
@@ -111,31 +97,26 @@ const state = {
 
         if (email) {
 
-            try {
+            const allMascotsByAUser = await fetch(API_BASE_URL + "/user/reported-mascots" + "?email=" + email, {
+            })
+            .then((res) => { return res.json(); })
+            .then((data) => {
 
-                const allMascotsByAUser = await fetch(API_BASE_URL + "/user/reported-mascots" + "?email=" + email, {
-                })
-                .then((res) => { return res.json(); })
-                .then((data) => {
-    
-                    if (data) {
-                        console.log("Esta es la data de todas las mascotas reportadas por un usuario: ", data);
-                        currentState["myReportedPets"] = data;
-                        state.setState(currentState);
-                        
-                    } else {
-                        console.log("No reportaste mascotas");
-                    }
-                    return data;
-                })
-                // .catch((err) => {
-                    // console.log("Este es el error de mascots reported by a user: ", err);
-                // });
-                callback();
-    
-            } catch (err) {
+                if (data) {
+
+                    console.log("Esta es la data de todas las mascotas reportadas por un usuario: ", data);
+                    currentState["myReportedPets"] = data;
+                    state.setState(currentState);
+                    
+                } else {
+                    console.log("No reportaste mascotas");
+                }
+            })
+            .catch((err) => {
                 console.log("Este es el error de mascots reported by a user: ", err);
-            }
+            });
+            
+            callback();
 
         } else {
             console.error('Falta el email');
