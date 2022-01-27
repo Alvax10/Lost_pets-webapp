a1d295df72bae649562b5ae37b7a318d61aa2f6e
Alvax10 <alvaro695547@gmail.com>
2022-01-27 16:07:59 -0300

    heroku api url

--- fe-src/state.ts
+++ fe-src/state.ts
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:3011";
-// const API_BASE_URL = "https://desafio-final-dwf-m7.herokuapp.com";
+// const API_BASE_URL = "http://localhost:3011";
+const API_BASE_URL = "https://desafio-final-dwf-m7.herokuapp.com";
 
 const state = {
     data: {
