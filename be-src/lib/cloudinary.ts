0d1dfd5910cd772c01a379ec01e1bf879becfebd
Alvax10 <alvaro695547@gmail.com>
2022-03-03 12:45:09 -0300

    fixing cloudinary problems

--- be-src/lib/cloudinary.ts
+++ be-src/lib/cloudinary.ts
@@ -2,9 +2,14 @@ import { v2 as cloudinary } from 'cloudinary';
 import "dotenv/config";
 
 cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME_CLOUDINARY, 
+    cloud_name: 'dx1fmmltu', 
     api_key: process.env.API_KEY_CLOUDINARY,
     api_secret: process.env.API_SECRET_CLOUDINARY,
 });
+
+console.log({
+    API_KEY_CLOUDINARY: process.env.API_KEY_CLOUDINARY,
+    API_SECRET_CLOUDINARY: process.env.API_SECRET_CLOUDINARY,
+})
 
 export { cloudinary };
