ee53a538ece2dd79728a207c867a0ba72d58b7de
Alvax10 <alvaro695547@gmail.com>
2022-03-28 13:41:03 -0300

    fixing editMascot

--- be-src/controllers/mascot-controller.ts
+++ be-src/controllers/mascot-controller.ts
@@ -146,7 +146,7 @@ export async function updateProfile(mascotId, objectID, petName, ImageDataURL, m
             }
 
             console.log("MascotLocation: ", mascotLocation);
-            if (mascotLocation !== null) {
+            if (mascotLocation["name"] !== null && mascotLocation["lat"] !== null && mascotLocation["lng"] !== null) {
 
                 const mascotUpdated = await index.partialUpdateObject({
                     objectID: objectID,
